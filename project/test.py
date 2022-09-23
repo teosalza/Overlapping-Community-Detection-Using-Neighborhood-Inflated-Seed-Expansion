e69325efd3c2d5f818921a7c2fe055f2e87d39e8
matteo salzani <teosalza@hotmail.it>
2022-09-23 22:36:42 +0200

    last commit

--- project/test.py
+++ project/test.py
@@ -3,5 +3,10 @@ import os
 vect = ["1e-2","1e-8"]
 
 for el in vect:
-    os.system("python .\project\Main.py -i datasets\\HepPh\\ca-HepPh.mtx  -d HepPh -a 0.9 -e "+el+ " -o output.txt") 
+    os.system("python .\project\Main.py -i datasets\\AstroPh\\AstroPh.txt  -d AstroPh -a 0.9 -e "+el+ " -o output.txt") 
+
+
+
+
+    
 
